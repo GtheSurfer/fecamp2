2fdc81ae81963c12301ebfc5fb1fe548b554d1f6
dece24kr <jcm@JCMui-MacBook-Pro.local>
2015-03-05 21:55:07 +0900

    3.5.2

--- html/default.js
+++ html/default.js
@@ -1,4 +1,5 @@
 jQuery(function($){
+	// 모바일 GNB 토글 버튼
 	var $gnbBtn = $(".gnb>button");
 
 	$gnbBtn.on("click", function(){
@@ -12,4 +13,26 @@ jQuery(function($){
 			$gnbBtn.text("메뉴 열기");
 		}
 	});
+
+	// 태블릿 + 데스크톱 GNB 메뉴
+	$(".gnb>ul>li>a").on("mouseover focus", function(){
+		$(this).parent().addClass("active").siblings().removeClass("active");
+	});
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
